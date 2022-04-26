416ca26107675f2e0ef7094e108d4904167a6e0b
DennisNguyen97 <89007787+DennisNguyen97@users.noreply.github.com>
2022-04-26 23:45:19 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = "";
+const giftUrl = "https://www.facebook.com/denislil97/";
+const eventName = "Chúc Mừng Sinh Nhật Tiểu Thư";
+const titleCard = "Tặng Tiểu Thư";
+const contentCard =
+  "Trong lúc này, tư nghĩ về tiểu thư với tất cả yêu thương và tư ước rằng mọi thứ tư làm cho tiểu thư sẽ mang lại hạnh phúc cho tiểu thư trong ngày này và mãi mãi về sau. Chúc mọi điều đến với tiểu thư sẽ luôn là điều tuyệt vời nhất, chúc mỗi phút giây tiểu thư trải qua là những phút giây hạnh phúc nhất, ngọt ngào nhất. Chúc cho tụi mình vẫn mãi mãi bên cạnh nhau. Sinh nhật vui vẻ nha tiểu thư.";
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ:
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = "tieu-thu.jpg";
+const base64 = "";
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
